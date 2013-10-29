869ccf8ae1d1e2a40e960644edd34a89129f16c9
mr51 <sk8-github@bellks.com>
2013-10-29 18:22:04 +0900

    Add push variavle feature on DoAction.class.php#replaceValue

--- lib/Media/SWF/Tag/DoAction.class.php
+++ lib/Media/SWF/Tag/DoAction.class.php
@@ -28,10 +28,9 @@ class Media_SWF_Tag_DoAction extends Media_SWF_Tag
     $valname = null;
     while (true) {
       $action_code = $reader->getUI8();
-      $length = ($action_code & 0x80) ? $reader->getUI16LE() : 0; 
-    
+      $length = ($action_code & 0x80) ? $reader->getUI16LE() : 0;
       $contents = ($length > 0) ? $reader->getData($length) : null;
-    
+
       switch ($action_code) 
       {
         case 0x96: //PushData
@@ -64,7 +63,6 @@ class Media_SWF_Tag_DoAction extends Media_SWF_Tag
   public function build()
   {
     $writer = new IO_Bit();
-    
     foreach ($this->_tags as $index => $d)
     {
       $writer->putUI8($d['ActionCode']);
@@ -73,23 +71,49 @@ class Media_SWF_Tag_DoAction extends Media_SWF_Tag
       $writer->putUI16LE($d['Length']);
       $writer->putData($d['Content']);
     }
-    
     return $writer->output();
   }
 
   public function replaceValue($name, $value)
   {
+    // replace variable
     if (isset($this->_values[$name]))
     {
       $this->_values[$name]['Content'] = $value;
       $index = $this->_values[$name]['Index'];
 
       $data = "\x00" . mb_convert_encoding($value, 'sjis-win', 'utf-8') . "\x00";
-      $len = strlen($data); 
+      $len = strlen($data);
 
-      $this->_tags[$index]['Length']  = $len;
+      $this->_tags[$index]['Length'] = $len;
       $this->_tags[$index]['Content'] = $data;
       return true;
+    }
+
+    // push variable
+    if (true) {
+        $tags = $this->_tags;
+        $end_tag = array_pop($tags);
+        // push stack
+        $push_stack_data = array($name, $value,);
+        foreach ($push_stack_data as $push_stack_element) {
+            $data = "\x00" . $push_stack_element . "\x00";
+            $len = strlen($data);
+            $push_tag = array();
+            $push_tag['ActionCode'] = 0x96;
+            $push_tag['Length'] = $len;
+            $push_tag['Content'] = $data;
+            $tags[] = $push_tag;
+        }
+        // set valiable
+        $set_tag = array();
+        $set_tag['ActionCode'] = 0x1D;
+        $set_tag['Length'] = 0;
+        $set_tag['Content'] = null;
+        $tags[] = $set_tag;
+        $tags[] = $end_tag;
+        $this->_tags = $tags;
+        return true;
     }
     return false;
   }
